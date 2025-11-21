55c3e02f8050c2eae9c46cfcd1151b8ca6894607
Sarah Kenny <sarah@mtstesters.com>
2025-11-21 10:49:06 -0600

    minimal version for bug report

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,69 +1,133 @@
 <script setup lang="ts">
-import { sub } from 'date-fns'
-import type { DropdownMenuItem } from '@nuxt/ui'
-import type { Period, Range } from '~/types'
+import type { NavigationMenuItem } from '@nuxt/ui'
+import type { Member } from '~/types'
 
-const { isNotificationsSlideoverOpen } = useDashboard()
-
-const items = [[{
-  label: 'New mail',
-  icon: 'i-lucide-send',
-  to: '/inbox'
-}, {
-  label: 'New customer',
-  icon: 'i-lucide-user-plus',
-  to: '/customers'
-}]] satisfies DropdownMenuItem[][]
-
-const range = shallowRef<Range>({
-  start: sub(new Date(), { days: 14 }),
-  end: new Date()
-})
-const period = ref<Period>('daily')
+const links = [[{
+  label: 'Members',
+  icon: 'i-lucide-users',
+  to: '/',
+    children: [
+      {
+        label: "item 1",
+      },
+      {
+        label: "item 2",
+      },
+      {
+        label: "item 3",
+      }
+    ]
+},{
+  label: 'Members',
+  icon: 'i-lucide-users',
+  to: '/',
+    children: [
+      {
+        label: "item 1",
+      },
+      {
+        label: "item 2",
+      },
+      {
+        label: "item 3",
+      }
+    ]
+},{
+  label: 'Members',
+  icon: 'i-lucide-users',
+  to: '/',
+    children: [
+      {
+        label: "item 1",
+      },
+      {
+        label: "item 2",
+      },
+      {
+        label: "item 3",
+      }
+    ]
+},{
+  label: 'Members',
+  icon: 'i-lucide-users',
+  to: '/',
+    children: [
+      {
+        label: "item 1",
+      },
+      {
+        label: "item 2",
+      },
+      {
+        label: "item 3",
+      }
+    ]
+},{
+  label: 'Members',
+  icon: 'i-lucide-users',
+  to: '/',
+    children: [
+      {
+        label: "item 1",
+      },
+      {
+        label: "item 2",
+      },
+      {
+        label: "item 3",
+      }
+    ]
+},{
+  label: 'Members',
+  icon: 'i-lucide-users',
+  to: '/',
+    children: [
+      {
+        label: "item 1",
+      },
+      {
+        label: "item 2",
+      },
+      {
+        label: "item 3",
+      }
+    ]
+}]] satisfies NavigationMenuItem[][]
 </script>
 
 <template>
-  <UDashboardPanel id="home">
+  <UDashboardPanel id="settings" :ui="{ body: 'lg:py-12' }">
     <template #header>
-      <UDashboardNavbar title="Home" :ui="{ right: 'gap-3' }">
+      <UDashboardNavbar title="Settings">
         <template #leading>
           <UDashboardSidebarCollapse />
-        </template>
-
-        <template #right>
-          <UTooltip text="Notifications" :shortcuts="['N']">
-            <UButton
-              color="neutral"
-              variant="ghost"
-              square
-              @click="isNotificationsSlideoverOpen = true"
-            >
-              <UChip color="error" inset>
-                <UIcon name="i-lucide-bell" class="size-5 shrink-0" />
-              </UChip>
-            </UButton>
-          </UTooltip>
-
-          <UDropdownMenu :items="items">
-            <UButton icon="i-lucide-plus" size="md" class="rounded-full" />
-          </UDropdownMenu>
         </template>
       </UDashboardNavbar>
 
       <UDashboardToolbar>
-        <template #left>
-          <!-- NOTE: The `-ms-1` class is used to align with the `DashboardSidebarCollapse` button here. -->
-          <HomeDateRangePicker v-model="range" class="-ms-1" />
-
-          <HomePeriodSelect v-model="period" :range="range" />
-        </template>
+        <UNavigationMenu :items="links" highlight class="flex-1" />
       </UDashboardToolbar>
     </template>
 
     <template #body>
-      <HomeStats :period="period" :range="range" />
-      <HomeChart :period="period" :range="range" />
-      <HomeSales :period="period" :range="range" />
+      <div class="flex flex-col gap-4 sm:gap-6 lg:gap-12 w-full lg:max-w-2xl mx-auto">
+  <div>
+    <UPageCard
+      title="Members"
+      description="Invite new members by email address."
+      variant="naked"
+      orientation="horizontal"
+      class="mb-4"
+    >
+    </UPageCard>
+
+    <UPageCard variant="subtle" :ui="{ container: 'p-0 sm:p-0 gap-y-0', wrapper: 'items-stretch', header: 'p-4 mb-0 border-b border-default' }">
+
+      <div>
+        Some text to make this page seem less ridiculously empty.
+      </div>
+    </UPageCard>
+  </div>      </div>
     </template>
   </UDashboardPanel>
 </template>
